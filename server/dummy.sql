94732e081f056e951a5d0f2b9f5d3dd09944a121
Jesus Sanchez <jsanchez254@ucmerced.edu>
2018-11-06 19:40:37 -0800

    changed database

--- server/dummy.sql
+++ server/dummy.sql
@@ -1,3 +1,4 @@
+-- USER TABLE
 -- INSERT INTO User (u_firstName, u_lastName, u_email, u_userName, u_admin, u_main)
 
 -- VALUES ("Jesus", "Sanchez", "jsanchez254@ucmerced.edu", "kechuArceus", 1, "Ike"),
@@ -16,16 +17,35 @@
 --        ("Florin", "Rusu", "florin@ucmerced.edu", "florin1", 0, "Falco");
 
 
-INSERT INTO CommSect (cs_userID, cs_charID, cs_comment)
+-- COMMENT SECTION
+-- INSERT INTO CommSect (cs_userID, cs_charID, cs_comment)
 
-VALUES (1, 2 , "wow that character is super cool!"),
-       (1, 2 , "wow that character is super cool!"),
-       (1, 2 , "wow that character is super cool!"),
-       (1, 2 , "wow that character is super cool!"),
-       (1, 2 , "wow that character is super cool!"),
-       (1, 2 , "wow that character is super cool!"),
-       (1, 2 , "wow that character is super cool!"),
-       (1, 2 , "wow that character is super cool!"),
-       (1, 2 , "wow that character is super cool!"),
+-- VALUES (1, 2 , "wow that character is super cool!"),
+--        (2, 1 , "wow that character is super cool!"),
+--        (3, 2 , "wow that character is super cool!"),
+--        (4, 3 , "wow that character is super cool!"),
+--        (5, 4 , "wow that character is super cool!"),
+--        (6, 5 , "wow that character is super cool!"),
+--        (7, 6 , "wow that character is super cool!"),
+--        (8, 7 , "wow that character is super cool!"),
+--        (9, 8 , "wow that character is super cool!"),
+--        (10, 9 , "wow that character is super cool!"),
+--        (11, 10 , "wow that character is super cool!"),
+--        (12, 5 , "wow that character is super cool!"),
+--        (13, 4 , "wow that character is super cool!"),
+--        (14, 1 , "wow that character is super cool!");
 
-       
+-- VOTING
+INSERT INTO Voting (l_charID, l_like, l_dislike)
+
+VALUES  (1, 3, 1),
+        (2, 3, 1),
+        (3, 5, 2),
+        (4, 3, 9),
+        (5, 3, 1),
+        (6, 8, 1),
+        (7, 14, 0),
+        (8, 4, 0),
+        (9, 12, 1),
+        (10, 11, 0)
+
