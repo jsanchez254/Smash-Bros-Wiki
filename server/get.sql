04ae89d5c6f601e6bca8c3a685cb88fca5160a11
Jesus Sanchez <jsanchez254@ucmerced.edu>
2018-11-06 23:23:11 -0800

    added some queries

--- server/get.sql
+++ server/get.sql
@@ -1,6 +1,4 @@
 -- #---GET STATEMENTS----#
--- #get character with most likes
--- #get character's game
 -- #order characters DESC based on their chronological release date
 -- #get character's franchise
 -- #get character's moves!
@@ -9,3 +7,11 @@
 -- #get comments made about certain character (ask for char) display char and comment
 -- #check if user with same username or password is in the system
 
+-- 1 #get character with most likes
+-- select distinct c_name, l_like from Character, joinVandU, Voting  where
+--  c_charID = jvu_charID and l_charID = jvu_charID group by c_name 
+--  having l_like = (select max(l_like) from Voting);
+
+-- 2 #get character's game
+select g_name from Games, joinCandG, Character where g_gameID = j_gameID and c_charID = j_charID
+where c_name = "Link";
