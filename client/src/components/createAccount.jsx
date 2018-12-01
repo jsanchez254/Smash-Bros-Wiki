5d8aeb5339fa53615ed198283b35aff1b2c45fe5
Jesus Sanchez <jsanchez254@ucmerced.edu>
2018-11-30 21:02:52 -0800

    added changes to createAccount

--- client/src/components/createAccount.jsx
+++ client/src/components/createAccount.jsx
@@ -3,12 +3,17 @@ import React, { Component } from 'react';
 //IMPORT AXIOS
 import axios from "axios";
 
+//import logo
+import logo from "../assets/img/smashLogo.png";
+
 class createAccount extends Component {
     state = { 
         userName: "",
         password: "",
         main: "",
-        email:""
+        email:"",
+        name:"",
+        lname: ""
      }
 
      handleChange(event){
@@ -32,7 +37,54 @@ class createAccount extends Component {
     render() { 
         return (
             <React.Fragment>
-                <h1>HELLO I AM CREATE ACCOUNT</h1>
+               <section className = "hero is-dark is-fullheight">
+                        <div className  ="hero-body"> 
+                            <div className = "container">
+                                <div className = "columns is-centered">
+                                    <div className = "column is-8">
+                                        <form onSubmit = {this.handleSubmit} className = "box">
+                                            <div className="field has-text-centered">
+                                                <img src= {logo} width="167"/>
+                                            </div>
+                                            
+                                            <div className = "field ">
+                                                <div className = "columns">
+                                                    <div className = "column is-6">
+                                                        <label className = "label"> Name: </label>
+                                                        <div className = "control has-icons-left">
+                                                            <input name = "name" className = "input"
+                                                            onChange = {this.handleChange
+                                                            } placeholder = "Enter Name"/>
+                                                                <span className = "icon is-small is-left">
+                                                                    <i className="fas fa-envelope"></i>
+                                                                </span>
+                                                        </div>
+                                                    </div>
+
+                                                
+                                                    <div className = "column is-6">
+                                                        <label className = "label">Last Name: </label>
+                                                        <div className = "control has-icons-left">
+                                                            <input name = "lname" className = "input"
+                                                            onChange = {this.handleChange
+                                                            } placeholder = "Enter Last Name"/>
+                                                                <span className = "icon is-small is-left">
+                                                                    <i className="fas fa-envelope"></i>
+                                                                </span>
+                                                        </div>
+                                                    </div>
+                                                    
+                                                    
+                                                    
+
+                                                </div>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                </section>
             </React.Fragment>
           );
     }
